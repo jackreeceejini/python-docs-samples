ac852cba7cb978b66305c6b3ae6fbd730696f6a1
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-06 15:10:34 -0400

    added TestHandler

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -14,13 +14,25 @@
 
 import webapp2
 
-
+form = """<form method="post" action="/testform">
+<input name="q">
+<input type="submit">
+</form>
+"""
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Gute Nacht')
+        #self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(self.request)
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage), 
+    ('/testform', TestHandler)
 ], debug=True)
